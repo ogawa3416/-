ef7045ea97428cee6d9885403748299ba4fe26b0
Kohei Ogawa <ogawa@oysterworld.jp>
2018-08-24 13:07:09 +0900

    C#を学習した際のサンプルコード

---
+++ C#勉強用/chapter2-1/DistanceConverter/FeetConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DistanceConverter
+{
+    public static class FeetConverter
+    {
+        private const double ratio = 0.3048;
+
+        //メートルからフィートを求める
+        public static double FromMeter(double meter)
+        {
+            return meter / ratio;
+        }
+
+        //フィートからメートルを求める
+        public static double ToMeter(double feet)
+        {
+            return feet * ratio;
+        }
+    }
+}
---
+++ C#勉強用/chapter2-2/SalesCalculator/SalesCounter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesCalculator
+{
+    
+    //売り上げ集計クラス
+    public class SalesCounter
+    {
+        private List<Sale> _sales;
+
+        //コンストラクタ
+        public SalesCounter(List<Sale> sales)
+        {
+            _sales = sales;
+        }
+
+        //店舗別売り上げを求める
+        public Dictionary<string, int> GetPerStoreSales()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (Sale sale in _sales)
+            {
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ShopName] += sale.Amount;
+                else
+                    dict[sale.ShopName] = sale.Amount;
+            }
+            return dict;
+        }
+    }
+}
