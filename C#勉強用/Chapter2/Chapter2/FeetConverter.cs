73c4202677e636ebccce4d6fcf33027d4bdf1cbc
Kohei Ogawa <ogawa@oysterworld.jp>
2018-11-01 18:53:54 +0900

    C#のサンプルコード

---
+++ C#勉強用/Chapter2/Chapter2/FeetConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter2
+{
+    public static class FeetConverter
+    {
+        public static double FromMeter(double meter)
+        {
+            return meter / 0.3048;
+        }
+
+        public static double ToMeter(double feet)
+        {
+            return feet * 0.3048;
+        }
+    }
+}
