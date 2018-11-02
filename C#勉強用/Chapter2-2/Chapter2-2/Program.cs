0f062dc86f78c5ace67340245a398818b52eb8bc
Kohei Ogawa <ogawa@oysterworld.jp>
2018-11-02 18:46:32 +0900

    C#のサンプルコード

---
+++ C#勉強用/Chapter2-2/Chapter2-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using Chapter2_2.SalesCalculator;
+using System;
+
+namespace Chapter2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sales = new SalesCounter("Sales.csv");
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStore)
+            {
+                Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+            }
+        }
+    }
+}
---
+++ C#勉強用/Chapter2-2/Chapter2-2/Sale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter2_2
+{
+    public class Sale
+    {
+        // 店舗名
+        public string ShopName { get; set; }
+
+        // 商品カテゴリ
+        public string ProductCategory { get; set; }
+
+        // 売り上げ高
+        public int Amount { get; set; }
+    }
+}
