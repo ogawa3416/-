beacdf251e9f82b70ce0481316c7cf949d306a37
Kohei Ogawa <ogawa@oysterworld.jp>
2018-11-13 18:55:32 +0900

    C#のサンプルコード

---
+++ C#勉強用/Chapter4/Chapter4-practice2/Chapter4-practice2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4_practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var YearMonths = new YearMonth[]
+            {
+                new YearMonth(2000,2),
+                new YearMonth(2003,1),
+                new YearMonth(2010,12),
+                new YearMonth(2016,7),
+                new YearMonth(2018,11),
+            };
+
+            WriteLine(YearMonths);
+            Console.WriteLine("-------------------------");
+
+            Exercise2_4(YearMonths);
+            Console.WriteLine("-------------------------");
+
+            Exercise2_5(YearMonths);
+            Console.WriteLine("-------------------------");
+        }
+
+        public static void WriteLine(YearMonth[] ym)
+        {
+            foreach(var item in ym)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static YearMonth FindFirst21C(YearMonth[] ym)
+        {
+            foreach(var item in ym)
+            {
+                if (item.Is21Century)
+                    return item;
+            }
+            return null;
+        }
+
+        public static void Exercise2_4(YearMonth[] ym)
+        {
+            var yearmonth = FindFirst21C(ym);
+            if (yearmonth == null)
+                Console.WriteLine("21世紀のデータはありません");
+            else
+                Console.WriteLine(yearmonth);
+        }
+
+        public static void Exercise2_5(YearMonth[] ym)
+        {
+            var array = ym.Select(yms => yms.AddOneMonth())
+                                    .ToArray();
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ C#勉強用/Chapter4/Chapter4-practice2/Chapter4-practice2/YearMonth.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter4_practice2
+{
+    public class YearMonth
+    {
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+
+        public YearMonth(int year, int month)
+        {
+            this.Year = year;
+            this.Month = month;
+        }
+
+        public bool Is21Century
+        {
+            get
+            {
+                return 2001 <= Year && Year <= 2100;
+            }
+        }
+
+        public YearMonth AddOneMonth()
+        {
+            if (Month == 12)
+            {
+                return new YearMonth(this.Year + 1, 1);
+            }
+            else
+            {
+                return new YearMonth(this.Year, this.Month + 1);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Year}年{Month}月";
+        }
+    }
+}
